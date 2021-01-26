4c063f4ea1d4b29211f3f5e75995d198677d01f3
vlxd1slxv <skyvvader@gmail.com>
2021-01-26 02:23:39 +0200

    docker rmq

--- apps/hero/src/main.ts
+++ apps/hero/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { HeroModule } from './hero.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(HeroModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    HeroModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'cats_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+
+  await app.listenAsync();
 }
+
 bootstrap();
